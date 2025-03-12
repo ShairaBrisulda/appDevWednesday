7f99875c96c04644eab04de6417b064c98080383
ShairaBrisulda <ssbrisulda@paterostechnologicalcollege.edu.ph>
2025-03-12 21:13:49 +0800

    Update GroupWork.java

--- GroupWork.java
+++ GroupWork.java
@@ -13,7 +13,7 @@ public class GroupWork   {
         System.out.println("subtract: " + Calculator.add(num1, num2));  // Group 2's method
         
 
-       System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
+       System.out.println("Are numbers Subtract? " + Calculator.isSubtract(num1, num2));  // Group 2's method
         scanner.close();
     }
 }
